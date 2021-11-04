7a80f1511767d5bd9b7829834984166a5d99b28d
MartinaBublincova <martinabublincova@gmail.com>
2021-11-04 20:06:27 +0100

    chore: footer margin

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
-function Footer () {
-    return (
-        <div>
-            <p style={{position: "absolute", bottom: "5px", width:"100%", textAlign: "center" }}>Copyright &copy; {new Date().getFullYear()} Martina Bublincova</p>
-        </div>
-    )
+function Footer() {
+  return (
+    <div>
+      <p
+        style={{
+          marginTop: "50%",
+          position: "absolute",
+          bottom: "5px",
+          width: "100%",
+          textAlign: "center",
+        }}
+      >
+        Copyright &copy; {new Date().getFullYear()} Martina Bublincova
+      </p>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
