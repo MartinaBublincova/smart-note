b0934652faa6795a83081249df248490bbc897d5
MartinaBublincova <martinabublincova@gmail.com>
2021-11-04 20:18:01 +0100

    chore: delete footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,7 @@
 import React from "react";
 
 function Footer() {
-  return (
-    <div>
-      <p
-        style={{
-          marginTop: "50%",
-          position: "absolute",
-          bottom: "5px",
-          width: "100%",
-          textAlign: "center",
-        }}
-      >
-        Copyright &copy; {new Date().getFullYear()} Martina Bublincova
-      </p>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default Footer;
